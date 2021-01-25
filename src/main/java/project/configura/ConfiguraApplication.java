b10c9b475575849e7ae6ec37798408fb999ecd72
Miguel <junior7395@hotmail.es>
2021-01-25 12:43:46 +0100

    Artifact spring boot

---
+++ src/main/java/project/configura/ConfiguraApplication.java
@@ -0,0 +1,13 @@
+package project.configura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguraApplication.class, args);
+	}
+
+}
